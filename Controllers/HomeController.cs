27280694a92522c217fcb36a8cc24b762ce23b38
satyanarayana.ziraff <satyanarayana.ziraff@gmail.com>
2020-03-01 15:50:04 +0530

    Commit for Create Action

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Pandu.Models;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Pandu.Controllers
 {
@@ -22,32 +24,62 @@ namespace Pandu.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Login(Usertable u)
         {
-            if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
-                using(janiEntities jb=new janiEntities())
+                using (janiEntities jb = new janiEntities())
                 {
                     var v = jb.Usertables.Where(a => a.Username.Equals(u.Username) && a.Password.Equals(u.Password)).FirstOrDefault();
-                    if(v!=null)
+                    if (v != null)
                     {
                         Session["LogedUsername"] = v.Username.ToString();
                         Session["Loged Password"] = v.Password.ToString();
                         return RedirectToAction("AfterLogin");
                     }
+
+
                 }
+
+     
             }
             return View(u);
         }
+
         public ActionResult AfterLogin()
         {
-            if(Session["LogedUsername"]!=null)
+
+            if (Session["LogedUsername"] != null)
+
             {
                 return View();
             }
             else
             {
-                return RedirectToAction("index");
+                return RedirectToAction("Create");
             }
-           
+
+            return View();
+
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create(string username,string password)
+        {
+            SqlConnection con = new SqlConnection("user id=sa;password=abc;database=jani;data source=.");
+            con.Open();
+          //  string query = "insert into usertable values(@username,@password)";
+            SqlCommand cmd = new SqlCommand("addusertable", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@username", username);
+            cmd.Parameters.AddWithValue("@password", password);
+            return View();
         }
     }
 }
+
+
+
+
+
